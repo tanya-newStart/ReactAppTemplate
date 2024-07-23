45767e625b0eec4913dcaeb209fe9a8aa73488ad
Tanya Dewland <tanya.dewland@gmail.com>
2024-07-23 17:41:55 +0200

    Make it basic

--- firstReactApp/src/App.jsx
+++ firstReactApp/src/App.jsx
@@ -1,8 +1,11 @@
 function App() {
   return (
-    <h1 className="text-3xl text-center text-blue">
-      Welcome to Vite, React, Tailwind Template
-    </h1>
+    <>
+      <h1 className="text-3xl text-center text-red-700">
+        Welcome to Vite with TailwindCSS and React
+      </h1>
+    </>
   );
 }
+
 export default App;
